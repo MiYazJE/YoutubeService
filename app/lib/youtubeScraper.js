6a0b053c5fb44b14efb65d57e3baf81a62a74834
MiYazJE <ruben.saizserrano@gmail.com>
2020-08-30 18:18:52 +0200

    add duration

--- app/lib/youtubeScraper.js
+++ app/lib/youtubeScraper.js
@@ -14,25 +14,26 @@ module.exports = {
 };
 
 async function scrapFirstVideo(query) {
-	await page.goto(`https://www.youtube.com/results?search_query=${query}`);
-	await page.waitForSelector('#thumbnail');
+	await page.goto(`https://www.youtube.com/results?search_query=${query}`, { waitUntil: 'networkidle0' });
 	const song = await page.evaluate(() => {
 		const title = document.querySelector('ytd-video-renderer yt-formatted-string').innerText;
-		const url = `https://www.youtube.com${document.querySelector('#thumbnail').getAttribute('href')}`;
+		const duration = document.querySelector('ytd-video-renderer ytd-thumbnail-overlay-time-status-renderer span');
+		const url = `https://www.youtube.com${document.querySelector('ytd-video-renderer #thumbnail').getAttribute('href')}`;
 		const thumbnail = document.querySelector('ytd-video-renderer #img').getAttribute('src');
-		return { title, url, thumbnail };
+		return { title, url, thumbnail, duration: duration ? duration.innerText : null };
 	});
 	return song;
 }
 
 async function scrapVideos(query) {
-    await page.goto(`https://www.youtube.com/results?search_query=${query}`, { waitUntil: 'networkidle0' });
+	await page.goto(`https://www.youtube.com/results?search_query=${query}`, { waitUntil: 'networkidle0' });
     const videos = await page.evaluate(() => {
-        return [...document.querySelectorAll('ytd-video-renderer')].map((el) => {
-            const title = el.querySelector('#video-title').innerText;
+		return [...document.querySelectorAll('ytd-video-renderer')].map((el) => {
+			const title = el.querySelector('#video-title').innerText;
+			const duration = el.querySelector('ytd-thumbnail-overlay-time-status-renderer span');
             const urlThumbnail = el.querySelector('#img').getAttribute('src');
             const url = el.querySelector('#thumbnail').getAttribute('href');
-            return { title, urlThumbnail, url };
+            return { title, urlThumbnail, url, duration: duration ? duration.innerText : null };
         });
     });
     return videos.filter(v => v.urlThumbnail);
